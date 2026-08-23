d039c0ce2cf789246690f3162761a42c716a4732
Uzma Yakub <uzmayakub27@gmail.com>
2026-08-23 19:45:44 +0600

    Delete src/model directory

--- src/model/Course.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class Course {
-    public int id;
-    public String title;
-    public String description;
-    public double price;
-}
--- src/model/Enrollment.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Enrollment {
-    public int userId;
-    public int courseId;
-    public double progress;
-}
