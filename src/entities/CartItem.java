597f9c612c2517232ccce50f5b521ab7ae483964
Uzma Yakub <uzmayakub27@gmail.com>
2026-08-25 22:12:34 +0600

    Implement CartItem class with properties and methods
    Added fields for MenuItem and quantity, along with constructors, getters, setters, and a method to calculate total price.

--- src/entities/CartItem.java
+++ src/entities/CartItem.java
@@ -1,5 +1,39 @@
 package fridaytcbd.flavorista.entities;
 
 public class CartItem {
-    
+
+    private MenuItem menuItem;
+    private int quantity;
+
+    public CartItem() {
+    }
+
+    public CartItem(MenuItem menuItem, int quantity) {
+        this.menuItem = menuItem;
+        this.quantity = quantity;
+    }
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+
+    public void setMenuItem(MenuItem menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotalPrice() {
+        if (menuItem == null || quantity <= 0) {
+            return 0.0;
+        }
+
+        return menuItem.getPrice() * quantity;
+    }
 }
