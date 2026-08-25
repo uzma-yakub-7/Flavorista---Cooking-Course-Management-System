0125810ef1b0e8ec3830c8d97042230c0b12fa0f
Uzma Yakub <uzmayakub27@gmail.com>
2026-08-25 22:09:03 +0600

    Implement MenuItem class with properties and methods
    Added fields for id, name, and price with constructors and getters/setters.

--- src/entities/MenuItem.java
+++ src/entities/MenuItem.java
@@ -1,5 +1,46 @@
 package fridaytcbd.flavorista.entities;
 
 public class MenuItem {
-    
+
+    private Long id;
+    private String name;
+    private double price;
+
+    public MenuItem() {
+    }
+
+    public MenuItem(Long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public MenuItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
