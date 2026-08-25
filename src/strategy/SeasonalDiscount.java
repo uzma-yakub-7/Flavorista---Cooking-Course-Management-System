eba5a531ec1f89ed8dcb263a90ee3c75beabfea7
Uzma Yakub <uzmayakub27@gmail.com>
2026-08-25 22:17:52 +0600

    Implement SeasonalDiscount class with discount logic

--- src/strategy/SeasonalDiscount.java
+++ src/strategy/SeasonalDiscount.java
@@ -1,5 +1,11 @@
 package fridaytcbd.flavorista.strategy;
 
-public class SeasonalDiscount {
-    
+public class SeasonalDiscount extends DiscountStrategy {
+
+    private static final double DISCOUNT = 0.15;
+
+    @Override
+    public double getDiscountPercentage() {
+        return DISCOUNT;
+    }
 }
