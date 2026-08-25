52a7ab8ad179593cdbee3b2313975647a36614fe
Uzma Yakub <uzmayakub27@gmail.com>
2026-08-25 22:18:27 +0600

    Implement StudentDiscount class extending DiscountStrategy

--- src/strategy/StudentDiscount.java
+++ src/strategy/StudentDiscount.java
@@ -1,5 +1,11 @@
 package fridaytcbd.flavorista.strategy;
 
-public class StudentDiscount {
-    
+public class StudentDiscount extends DiscountStrategy {
+
+    private static final double DISCOUNT = 0.10;
+
+    @Override
+    public double getDiscountPercentage() {
+        return DISCOUNT;
+    }
 }
