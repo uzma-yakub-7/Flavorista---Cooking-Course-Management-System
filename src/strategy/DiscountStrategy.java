aabc87a985634a2ef4b0b076f7cbea63a5a8091b
Uzma Yakub <uzmayakub27@gmail.com>
2026-08-25 22:15:11 +0600

    Refactor DiscountStrategy to abstract class with methods
    Refactor DiscountStrategy class to be abstract and add methods for calculating discount and final price.

--- src/strategy/DiscountStrategy.java
+++ src/strategy/DiscountStrategy.java
@@ -1,5 +1,22 @@
 package fridaytcbd.flavorista.strategy;
 
-public class DiscountStrategy {
-    
+public abstract class DiscountStrategy {
+
+    public abstract double getDiscountPercentage();
+
+    public double calculateDiscount(double subtotal) {
+        if (subtotal <= 0) {
+            return 0.0;
+        }
+
+        return subtotal * getDiscountPercentage();
+    }
+
+    public double calculateFinalPrice(double subtotal) {
+        if (subtotal <= 0) {
+            return 0.0;
+        }
+
+        return subtotal - calculateDiscount(subtotal);
+    }
 }
