cb6cc35b66f47846218282854935866f38c3c0db
Uzma Yakub <uzmayakub27@gmail.com>
2026-08-30 21:19:20 +0600

    Add unit tests for RegularDiscount class

--- src/tests/strategy/RegularDiscountTest.java
+++ src/tests/strategy/RegularDiscountTest.java
@@ -1,1 +1,45 @@
+package fridaytcbd.flavorista.strategy;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegularDiscountTest {
+
+    @Test
+    void shouldReturnFivePercentDiscount() {
+        RegularDiscount discount = new RegularDiscount();
+
+        assertEquals(0.05, discount.getDiscountPercentage());
+    }
+
+    @Test
+    void shouldCalculateDiscount() {
+        RegularDiscount discount = new RegularDiscount();
+
+        assertEquals(50.0, discount.calculateDiscount(1000.0));
+    }
+
+    @Test
+    void shouldCalculateFinalPrice() {
+        RegularDiscount discount = new RegularDiscount();
+
+        assertEquals(950.0, discount.calculateFinalPrice(1000.0));
+    }
+
+    @Test
+    void shouldReturnZeroForZeroSubtotal() {
+        RegularDiscount discount = new RegularDiscount();
+
+        assertEquals(0.0, discount.calculateDiscount(0.0));
+        assertEquals(0.0, discount.calculateFinalPrice(0.0));
+    }
+
+    @Test
+    void shouldReturnZeroForNegativeSubtotal() {
+        RegularDiscount discount = new RegularDiscount();
+
+        assertEquals(0.0, discount.calculateDiscount(-100.0));
+        assertEquals(0.0, discount.calculateFinalPrice(-100.0));
+    }
+}
